d968676f8ed2ac323a626b947d42d146450cc242
Eric Ross <eross1@alumni.nd.edu>
2017-07-12 15:42:15 -0400

    Finished complete.R

--- RProgramming/Week2/complete.R
+++ RProgramming/Week2/complete.R
@@ -1,4 +1,12 @@
 complete <- function(directory, id = 1:332){
-    
-    
+    setwd(directory)
+    fnames <- dir(pattern='.csv')
+    complete_df <- data.frame(matrix(ncol=2,nrow=length(id)))
+    colnames(complete_df) <- c('id','nobs')
+    for (i in seq_along(id)){
+        data <- read.csv(fnames[id[i]])
+        complete_df$id[i] <- id[i]
+        complete_df$nobs[i] <- nrow(data[complete.cases(data),])
+    }
+    complete_df
 }
